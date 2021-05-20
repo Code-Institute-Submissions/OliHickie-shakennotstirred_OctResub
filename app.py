fc4013c602865a2a53f70815ce022968fc145378
OliHickie <oli.h@hotmail.co.uk>
2021-05-20 16:05:47 +0000

    add login functionality

--- app.py
+++ app.py
@@ -29,6 +29,35 @@ def recipes():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
+        # check if user exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check password matches stored password
+            if check_password_hash(existing_user["password"], request.form.get(
+                    "password")):
+                session["user"] = request.form.get("username").lower()
+
+                return redirect(url_for(
+                    "mycocktails", username=session["user"]))
+
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
@@ -56,13 +85,8 @@ def login():
         session["user"] = request.form.get("username").lower()
 
         # change to my recipes once built <----------------------
-        return redirect(url_for("recipes", username=session["user"]))
+        return redirect(url_for("mycocktails", username=session["user"]))
 
-    return render_template("login.html")
-
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
     return render_template("login.html")
 
 
